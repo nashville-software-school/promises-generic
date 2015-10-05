53a14c2312c7bf9c04471706629bf10396732f39
Brian Merchant <btmerchant@comcast.net>
2015-10-05 16:15:10 -0500

    Files hashed up commit before pull

--- javascripts/get-books.js
+++ javascripts/get-books.js
@@ -1,32 +1,103 @@
 define(function(require) {
   var _ = require("lodash");
-  var $ = require("jquery");
-  var q = require("q");
 
+  return {
+    load: function(fn) {
 
-      // This XHR should be in its own require module, not here
-      // $.ajax("https://nss-book-store.firebaseio.com/booktypes.json").done(function(types) {
+        // This XHR does belong here
+        $.ajax("https://nss-book-store.firebaseio.com/books.json").done(function(books) {
 
-        // // This XHR does belong here
-        // $.ajax("https://nss-book-store.firebaseio.com/books.json").done(function(books) {
+          books = Object.keys( books ).map(key => books[ key ]);
 
-          return {
-            loadBooks: function () {
-            var deferred = q.defer();
+          /*
+            I'm using the lodash `find()` method here.
+              https://lodash.com/docs#find
+           */
+          var books = books.map(book => {
+            book.type = _.find(types, { id:book.booktype }).label;
+            return book;
+          });
 
-            $.ajax({
-              url:"https://nss-book-store.firebaseio.com/books.json"}).done(function(books){
-                deferred.resolve(books);
-                console.log(books);
-                }).fail(function(xhr, status, error){
-                  deferred.reject(error);
-                });
+          // Still relying on a callback? That's so 2014...
+          fn(books);
 
-               return deferred.promise;
+        });
 
-               }
+    }
   };
 });
+
+
+// define(function(require) {
+//   var q = require('../lib/bower_components/q/q');
+
+//   return {
+//     load: function() {
+//       var deferred = q.defer();
+
+//       $.ajax("https://nss-book-store.firebaseio.com/books.json")
+//       .done(function(booksObject) {
+//         console.log("booksObject", booksObject);
+//         deferred.resolve(booksObject);
+//       }).fail(function() {
+//         console.log("getBooks.load failed");
+//       });
+//       return deferred.promise;
+//     }
+//   };
+// });
+
+
+
+
+
+
+
+
+
+
+
+// define(function(require) {
+//   var _ = require("lodash");
+//   var $ = require("jquery");
+//   var q = require("q");
+
+
+//       // This XHR should be in its own require module, not here
+//       // $.ajax("https://nss-book-store.firebaseio.com/booktypes.json").done(function(types) {
+
+//         // // This XHR does belong here
+//         // $.ajax("https://nss-book-store.firebaseio.com/books.json").done(function(books) {
+
+//           return {
+//             loadBooks: function () {
+//             var deferred = q.defer();
+
+//             $.ajax({
+//               url:"https://nss-book-store.firebaseio.com/books.json"}).done(function(books){
+//                 deferred.resolve(books);
+//                 console.log("Books",books);
+//                 }).fail(function(xhr, status, error){
+//                   deferred.reject(error);
+//                 });
+//                 // debugger;
+//                 console.log("Ajax Call books Promise Returned = ",deferred.promise);
+//                return deferred.promise;
+
+//                }
+//   };
+// });
+
+
+
+
+
+
+
+
+
+
+
           /*
             This code is dependent upon two XHRs and violates
             the Single Responsibility Principle.
--- javascripts/xhr1.js
+++ javascripts/xhr1.js
@@ -1,24 +1,26 @@
-define (['jquery', 'q'], function ($, q) {
+define(function(require) {
+  var $ = require("jquery");
+  var q = require("q");
+
 
 return {
   bookType: function () {
   var deferred = q.defer();
+  console.log("Entered x1");
 
   $.ajax({
     url: "https://nss-book-store.firebaseio.com/booktypes.json"
+    types = Object.keys( types ).map(key => types[ key ]);
   }).done(function(types) {
     deferred.resolve(types);
-    console.log(types);
+    console.log("Ajax Call Types = ",types);
   }).fail(function(xhr, status, error){
     deferred.reject(error);
   });
-
+  console.log("Ajax Call Types Promise Returned = ",deferred.promise);
   return deferred.promise;
-  // console.log(deferred.promise);
 
-  }
-};
 
-// console.log(bookType);
-
+    }
+  };
 });
